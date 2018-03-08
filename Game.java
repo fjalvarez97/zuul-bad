7c6616a0c14dcbd9ff572bbd46a39e2f7d8039b8
fjalvarez97 <franjalvarez955@gmail.com>
2018-03-08 17:02:09 +0100

    creado el mapa de un banco con 6 salas

--- Game.java
+++ Game.java
@@ -19,7 +19,7 @@ public class Game
 {
     private Parser parser;
     private Room currentRoom;
-        
+
     /**
      * Create the game and initialise its internal map.
      */
@@ -34,23 +34,25 @@ public class Game
      */
     private void createRooms()
     {
-        Room outside, theater, pub, lab, office;
-      
-        // create the rooms
-        outside = new Room("outside the main entrance of the university");
-        theater = new Room("in a lecture theater");
-        pub = new Room("in the campus pub");
-        lab = new Room("in a computing lab");
-        office = new Room("in the computing admin office");
-        
-        // initialise room exits
-        outside.setExits(null, theater, lab, pub);
-        theater.setExits(null, null, null, outside);
-        pub.setExits(null, outside, null, null);
-        lab.setExits(outside, office, null, null);
-        office.setExits(null, null, null, lab);
+        Room entrada, recepcion, salaMaquinas, oficinas, almacen, cajaFuerte;
 
-        currentRoom = outside;  // start game outside
+        // create the rooms
+        entrada = new Room("inside the main entrance of the bank");
+        recepcion = new Room("in the reception");
+        salaMaquinas = new Room("in the machine room");
+        oficinas = new Room("in the offices");
+        almacen = new Room("in the warehouse");
+        cajaFuerte = new Room("in the bank safety deposit box room!");
+
+        // initialise room exits
+        entrada.setExits(recepcion, null, null, null);
+        recepcion.setExits(almacen, oficinas, entrada, salaMaquinas);
+        salaMaquinas.setExits(null, recepcion, null, null);
+        oficinas.setExits(cajaFuerte, null, null, recepcion);
+        almacen.setExits(null, cajaFuerte, recepcion, null);
+        cajaFuerte.setExits(null, null, oficinas, almacen);
+
+        currentRoom = entrada;  //Empieza el juego en la entrada del banco.
     }
 
     /**
@@ -62,7 +64,7 @@ public class Game
 
         // Enter the main command loop.  Here we repeatedly read commands and
         // execute them until the game is over.
-                
+
         boolean finished = false;
         while (! finished) {
             Command command = parser.getCommand();
@@ -73,7 +75,7 @@ public class Game
 
     /**
      * Print out the opening message for the player.
-     */
+     */ 
     private void printWelcome()
     {
         System.out.println();
